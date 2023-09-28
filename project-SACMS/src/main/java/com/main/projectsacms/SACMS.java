0ffc4609e786715005fd08bc2e1e158938e0ee1c
Sandin Manmitha Maheeshakya <sadinkasm@gmail.com>
2023-09-28 20:05:40 +0530

    Additional Changes

--- project-SACMS/src/main/java/com/main/projectsacms/SACMS.java
+++ project-SACMS/src/main/java/com/main/projectsacms/SACMS.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SACMS extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(SACMS.class.getResource("dashboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720 );
+        stage.setTitle("ClubSync");
         stage.setScene(scene);
         stage.show();
     }
