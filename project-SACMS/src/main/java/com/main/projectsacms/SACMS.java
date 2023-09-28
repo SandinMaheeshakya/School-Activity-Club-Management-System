596f9d77912ad49465728f33034e4e0494897df8
Sandin Manmitha Maheeshakya <sadinkasm@gmail.com>
2023-09-28 20:10:15 +0530

    changing the Club name to ClubData

--- project-SACMS/src/main/java/com/main/projectsacms/SACMS.java
+++ project-SACMS/src/main/java/com/main/projectsacms/SACMS.java
@@ -12,7 +12,7 @@ public class SACMS extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SACMS.class.getResource("dashboard.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720 );
-        stage.setTitle("ClubSync");
+        stage.setTitle("ClubData");
         stage.setScene(scene);
         stage.show();
     }
