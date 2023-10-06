dba6dc19b5b502438c0f5d241587eb8081829604
Vanuja Sooriyaarachchi <146374517+VanujaSooriyaarachchi@users.noreply.github.com>
2023-10-06 08:56:50 +0530

    new file

--- project-SACMS/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.main.projectsacms {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                            
-    opens com.main.projectsacms to javafx.fxml;
-    exports com.main.projectsacms;
-}
