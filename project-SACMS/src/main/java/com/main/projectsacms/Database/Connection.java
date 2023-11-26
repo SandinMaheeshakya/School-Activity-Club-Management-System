871714566efc158d8dd8cc8c8c2d20e61bab6129
Sandin Manmitha Maheeshakya <sadinkasm@gmail.com>
2023-11-26 12:01:09 +0530

    smaller change in DB connection

--- project-SACMS/src/main/java/com/main/projectsacms/Database/Connection.java
+++ project-SACMS/src/main/java/com/main/projectsacms/Database/Connection.java
@@ -11,7 +11,7 @@ public abstract class Connection {
 
     protected static java.sql.Connection connection = null;
 
-    public static java.sql.Connection establishConnection() {
+    public static void establishConnection() {
 
         try {
              connection = DriverManager.getConnection(DB_URL, DB_Username, DB_Password);
@@ -20,7 +20,6 @@ public abstract class Connection {
             e.printStackTrace();
 
         }
-        return connection;
     }
 
     public static void closeConnection(){
